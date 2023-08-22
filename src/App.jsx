06cf7e3858c452553336adb618b5835121e7dd12
Angel Ramirez (Jars) <ra374885@uaeh.edu.mx>
2023-08-21 22:01:47 -0600

    Update Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
+import Header from './assets/components/Header/Header'
 
 function App() {
   return (
     <>
       <section className='w-full h-screen'>
-        <div className='flex h-full justify-center items-center'>
-          <h1 className='text-4xl font-bold text-red-800'>
-            Hello World!
-          </h1>
-        </div>
+        <Header />
       </section>
     </>
   )
--- src/assets/components/Header/Header.jsx
+++ src/assets/components/Header/Header.jsx
@@ -3,7 +3,28 @@ import React from 'react'
 function Header() {
   return (
     <>
-        
+        <div className='w-full h-14 bg-orange-200'>
+            <div className='flex h-full justify-between items-center mx-10'>
+                <div>   
+                    <div>
+                        <ul className='flex space-x-6'>
+                            <li>Home</li>
+                            <li>Home</li>
+                            <li>Home</li>
+                            <li>Home</li>
+                        </ul>
+                    </div>
+                </div>
+                <div>
+                    <div className='flex space-x-6'>
+                        <h1>X</h1>
+                        <h1>
+                            Main Menu
+                        </h1>
+                    </div>
+                </div>
+            </div>
+        </div>
     </>
   )
 }
