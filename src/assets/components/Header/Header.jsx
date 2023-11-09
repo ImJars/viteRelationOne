278e6ed2bea4d69b562015a1a2dff55c50cec32c
Angel Ramirez (Jars) <ra374885@uaeh.edu.mx>
2023-11-08 23:40:09 -0600

    Add Home-animation

--- src/assets/components/Header/Header.jsx
+++ src/assets/components/Header/Header.jsx
@@ -1,7 +1,22 @@
-import {React, useState} from "react";
+import { React, useState } from "react";
 import { TbLetterP, TbLetterJ } from "react-icons/tb";
 import { HiOutlineMenuAlt1 } from "react-icons/hi";
 import { AiOutlineClose } from "react-icons/ai";
+import { motion } from "framer-motion";
+
+const fadeInAnimationVariants = {
+  initial: {
+    opacity: 0,
+  },
+  animateHeader: {
+    opacity: 1,
+    transition: {
+      delay: 2,
+      duration: 0.5,
+      transition: "easeInOut",
+    },
+  },
+};
 
 function Header() {
   const [showMenu, setShowMenu] = useState(false);
@@ -12,30 +27,44 @@ function Header() {
   return (
     <>
       <div className="fixed w-full h-14 bg-transparent z-50">
-        <div className="flex h-full justify-between items-center mx-5 my-2">
+        <motion.div
+          variants={fadeInAnimationVariants}
+          initial="initial"
+          whileInView="animateHeader"
+          viewport={{
+            once: true,
+          }}
+          className="flex h-full justify-between items-center mx-5 my-2"
+        >
           <div>
             <div>
               <ul className="flex space-x-6 items-center">
                 <div className="flex space-x-5">
-                    <div className="flex items-center text-white opacity-30">
-                        <TbLetterP className="text-4xl"/>
-                        <h1>&</h1>
-                        <TbLetterJ className="text-4xl"/>
-                    </div>
+                  <div className="flex items-center text-white opacity-20">
+                    <TbLetterP className="text-4xl" />
+                    <h1>&</h1>
+                    <TbLetterJ className="text-4xl" />
+                  </div>
                 </div>
               </ul>
             </div>
           </div>
           <div>
             <button className="flex space-x-6">
-              {showMenu ?
-                <AiOutlineClose className="text-white text-4xl" onClick={toggleMenu}/>
-                :
-                <HiOutlineMenuAlt1 className="text-white text-4xl" onClick={toggleMenu}/>
-              }
+              {showMenu ? (
+                <AiOutlineClose
+                  className="text-white text-4xl"
+                  onClick={toggleMenu}
+                />
+              ) : (
+                <HiOutlineMenuAlt1
+                  className="text-white text-4xl"
+                  onClick={toggleMenu}
+                />
+              )}
             </button>
           </div>
-        </div>
+        </motion.div>
       </div>
     </>
   );
--- src/assets/components/Home/container.jsx
+++ src/assets/components/Home/container.jsx
@@ -1,26 +1,117 @@
 import React from "react";
 import RotatingText from "../rotatingText";
+import { motion } from "framer-motion";
+
+const fadeInAnimationVariants = {
+  initial: {
+    opacity: 0,
+  },
+  initialImg: {
+    blur: 0,
+  },
+  animateImg: {
+    blur: 2,
+    transition: {
+      delay: 5,
+      duration: 0.5,
+      transition: "easeInOut",
+    },
+  },
+  animateTitleOne: {
+    opacity: 1,
+    transition: {
+      delay: 4,
+      duration: 0.5,
+      transition: "easeInOut",
+    },
+  },
+  animatedY: {
+    opacity: 1,
+    transition: {
+      delay: 5,
+      duration: 0.5,
+      transition: "easeInOut",
+    },
+  },
+  AnimateTitleTwo: {
+    opacity: 1,
+    transition: {
+      delay: 6,
+      duration: 0.5,
+      transition: "easeInOut",
+    },
+  },
+  AnimateRotating: {
+    opacity: 1,
+    transition: {
+      delay: 7,
+      duration: 0.5,
+      transition: "easeInOut",
+    },
+  },
+};
 
 function Container() {
   return (
     <>
       <section className="w-full h-screen font-sans">
         <div className="w-full h-screen justify-center bg-wine items-center">
-            <img className="absolute w-full object-cover h-screen brightness-[0.4] lg:brightness-[0.3] blur lg:blur-sm" src="home.jpeg" alt="" />
+          <motion.img
+            variants={fadeInAnimationVariants}
+            initial="initialImg"
+            whileInView="animateImg"
+            viewport={{
+              once: true,
+            }}
+            className="absolute w-full object-cover h-screen brightness-[0.4] lg:brightness-[0.3] blur lg:blur-sm"
+            src="home.jpeg"
+            alt=""
+          />
           <div className="w-full h-full flex flex-col justify-center items-center">
-            <h1 className="text-8xl font-bold text-white transform rotate-90 -translate-x-12 translate-y-12 text-">
+            <motion.h1
+              variants={fadeInAnimationVariants}
+              initial="initial"
+              whileInView="animateTitleOne"
+              viewport={{
+                once: true,
+              }}
+              className="text-8xl font-bold text-white transform rotate-90 -translate-x-12 translate-y-12 text-"
+            >
               PAULETTE
-            </h1>
-            <h1 className="text-2xl font-bold text-white transform text-center">
+            </motion.h1>
+            <motion.h1
+              variants={fadeInAnimationVariants}
+              initial="initial"
+              whileInView="animatedY"
+              viewport={{
+                once: true,
+              }}
+              className="text-2xl font-bold text-white transform text-center"
+            >
               &
-            </h1>
-            <h2 className="text-8xl font-bold text-white transform -rotate-90 translate-x-12 -translate-y-16">
+            </motion.h1>
+            <motion.h2
+              variants={fadeInAnimationVariants}
+              initial="initial"
+              whileInView="AnimateTitleTwo"
+              viewport={{
+                once: true,
+              }}
+              className="text-8xl font-bold text-white transform -rotate-90 translate-x-12 -translate-y-16"
+            >
               ANGEL
-            </h2>
+            </motion.h2>
           </div>
-          <RotatingText 
-            classNames={"absolute transform -translate-y-40"}
-        />
+          <motion.div
+            variants={fadeInAnimationVariants}
+            initial="initial"
+            whileInView="AnimateRotating"
+            viewport={{
+              once: true,
+            }}
+          >
+            <RotatingText classNames={"absolute transform -translate-y-40"} />
+          </motion.div>
         </div>
       </section>
     </>
